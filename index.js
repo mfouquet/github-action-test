261a8b0ee279cb522eba30b42e421305428f8962
Michael Fouquet <mfouquet@users.noreply.github.com>
2023-01-22 14:21:03 -0600

    :sparkles: Init of repo

---
+++ index.js
@@ -0,0 +1,12 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+try {
+  // Get the JSON webhook payload for the event that triggered the workflow
+  const payload = JSON.stringify(github.context.payload, undefined, 2)
+  const context = JSON.stringify(github.context, undefined, 2)
+  console.log(`The event payload: ${payload}`);
+  console.log(`The event context: ${context}`);
+} catch (error) {
+  core.setFailed(error.message);
+}
