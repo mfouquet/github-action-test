74837355e1cef4245aaf47eb7938fcaf7d1a983a
Michael Fouquet <mfouquet@users.noreply.github.com>
2023-01-22 15:08:39 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const fs = require('fs');
 
 try {
   // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  const context = JSON.stringify(github.context, undefined, 2)
-  console.log(`The event payload: ${payload}`);
-  console.log(`The event context: ${context}`);
-  console.log(process.argv);
-  console.log(process.env.FILES_CHANGED);
+  // console.log(process.env.FILES_CHANGED);
+
+  const files = JSON.parse(process.env.FILES_CHANGED);
+
+  files.forEach(file => {
+    console.log(file);
+  });
+  // fs.readFile('./example/example.css', 'utf8', (err, data) => {
+  //   if (err) {
+  //     console.error(err);
+  //     return;
+  //   }
+  //   console.log(data);
+  // });
+
 } catch (error) {
   core.setFailed(error.message);
 }
